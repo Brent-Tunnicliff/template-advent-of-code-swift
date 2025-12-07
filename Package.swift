6f3526a5c49027446f73903f9bd938b39c8e292f
Brent Tunnicliff <brent@tunnicliff.dev>
2025-12-07 19:29:54 +1100

    Increased macOS version and removed resolved file.
    This way a new resolved will generate with each project generated.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "AdventOfCode",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v26)],
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-algorithms.git",
